764819ddf3ee1a3ae32a9400e4145d9f809b92a1
YutoMizutani <yuto.mizutani.app@gmail.com>
2017-09-07 15:19:14 +0900

    Update FR_written_in_C.c

--- SimpleSchedule/FR/FR_written_in_C.c
+++ SimpleSchedule/FR/FR_written_in_C.c
@@ -1,8 +1,19 @@
+//
+//  FR_written_in_C.c
+//
+//  Created by Yuto Mizutani on 2017/09/07.
+//  Copyright (c) 2017 Yuto Mizutani.
+// 
+//  This software is released under the MIT License.
+//  https://github.com/YutoMizutani/SchedulesOfReinforcement/blob/master/LICENSE
+//
+
+// Function definition in Clang
 int decisionFR(int numOfResp, int value) {
   return numOfResp % value == 0;
 }
    	
-// Function call
+// Call a function
 int numOfResp = 10;
 int value = 5;
 printf("%d", decisionFR(numOfResp, value)); // 1
